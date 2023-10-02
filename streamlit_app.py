2dd58351c3a0410a193da7471ef450c5d343c814
chadadee <84585467+chadadee@users.noreply.github.com>
2023-10-02 16:43:57 -0400

    Update streamlit_app.py
    Added additional code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.header('Break fast Menu')
+streamlit.text('Upma or Dosa')
+streamlit.text('Oat Meal Upma with veggies')
+streamlit.text('Multigrain vegggies')
+streamlit.text('Boiled Eggs with banana')
