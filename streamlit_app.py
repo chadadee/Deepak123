a1ead4456ba3e9276a98fb49d039cb431a532899
chadadee <84585467+chadadee@users.noreply.github.com>
2023-10-02 16:48:51 -0400

    Update streamlit_app.py
    Added extra menu items to breakfast list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
 streamlit.header('Break fast Menu')
+streamlit.text('Omega 3 &' Blueberry Oatmeal')
+streamlit.text('kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
 streamlit.text('Upma or Dosa')
 streamlit.text('Oat Meal Upma with veggies')
 streamlit.text('Multigrain vegggies')
