f2062352085dc1d6f1a501a205d26d37d51f861e
chadadee <84585467+chadadee@users.noreply.github.com>
2023-10-04 00:18:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import requests
+import pandas
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My Mom''s New Healthy Dinner')
 streamlit.header('Break fast Favorites')
 streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
@@ -9,7 +13,6 @@ streamlit.text('🥣Oat Meal Upma with veggies')
 streamlit.text('🥗Multigrain vegggies')
 streamlit.text('🐔Boiled Eggs with banana')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -20,14 +23,12 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
-import requests 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 #take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as table 
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -42,3 +43,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding',add_my_fruit)
+my_cur.execute("insert into pc_rivery_db.public.FRUIT_LOAD_LIST VALUES ('from streamlit')")
